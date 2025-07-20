663a6fc6916fe2e643d046cc260f21b913c02a28
HEMANTH-B-G-01-03 <hemanthbg2569@gmail.com>
2025-07-20 10:53:56 +0530

    predictions

--- LSTM/evaluate_LSTM.py
+++ LSTM/evaluate_LSTM.py
@@ -14,3 +14,15 @@ y_test = np.load('y_test1.npy')
 # Reshape X_test to 3D for LSTM
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 
+# Normalize input data
+X_test = (X_test - X_test.mean(axis=0)) / X_test.std(axis=0)
+X_test = np.nan_to_num(X_test)
+
+# Evaluate the model on the test set
+test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=2)
+print(f"Test Loss: {test_loss}")
+print(f"Test Accuracy: {test_accuracy}")
+
+# Predictions for the test set
+y_test_pred_probs = model.predict(X_test)
+y_test_pred = (y_test_pred_probs > 0.5).astype(int).flatten()
